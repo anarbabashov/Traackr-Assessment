7a286e28447091b9be0405ebe0330383c9e3d3cc
Anar Babashov <anarbabashov.it@gmail.com>
2022-05-20 16:18:45 -0400

    Updated vue config file

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 // vue.config.js file to be placed in the root of your repository
 
 module.exports = {
+  devServer: {
+    disableHostCheck: true,
+  },
   pwa: {
     workboxOptions: {
       exclude: [/_redirects/],
