b246a10d827be4d681452ccd8cff53278e3e1bb3
Anar Babashov <anarbabashov.it@gmail.com>
2022-05-20 15:59:15 -0400

    Added extra commad

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 // vue.config.js file to be placed in the root of your repository
-const mode =
-  process.env.NODE_ENV === 'development' ? 'development' : 'production';
 
 module.exports = {
-  mode,
   pwa: {
     workboxOptions: {
       exclude: [/_redirects/],
