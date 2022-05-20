85160e4a18790345913221260d56a4249fe0e22e
Anar Babashov <anarbabashov.it@gmail.com>
2022-05-20 15:53:24 -0400

    Added env file

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 // vue.config.js file to be placed in the root of your repository
+const mode =
+  process.env.NODE_ENV === 'development' ? 'development' : 'production';
 
 module.exports = {
+  mode,
   pwa: {
     workboxOptions: {
       exclude: [/_redirects/],
